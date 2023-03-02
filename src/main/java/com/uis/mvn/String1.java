34eeeebeee6224884f8ac75de298251e8dc920cd
varsha <varsha95155@gmail.com>
2023-03-02 13:07:46 +0530

    Adding project structure

---
+++ src/main/java/com/uis/mvn/String1.java
@@ -0,0 +1,31 @@
+package com.uis.mvn;
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+public class String1 {
+
+	public static void main(String[] args) {
+          Scanner kb=new Scanner(System.in);
+          System.out.println("enter the name");
+          String name=kb.nextLine();
+ 	is20Character(name);
+ 	kb.close();
+	}
+     public static void is20Character(String name) {
+    	 if (name.length()==20) {
+    			System.out.println(name);
+    		} else if(name.length()>20) {
+    			String dupName=name;
+    	      for (int i = 1; i <=(name.length()-20); i++) {
+    			dupName=StringUtils.chop(dupName);
+    		}
+    	      System.out.println(dupName);
+    		}
+    		else if(name.length()<20) {
+    			for (int i = 1; i <=(20-(name.length())); i++) {
+    				System.out.print("*");
+    			}
+    			System.out.println(name);
+    		}
+      }
+     
+}
