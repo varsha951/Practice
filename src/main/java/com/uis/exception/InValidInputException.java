e2e17e5fc6e6bf615f1c0e08b7226da2b64b5611
varsha <varsha95155@gmail.com>
2023-03-03 14:10:09 +0530

    throws exception for invalid inputs

---
+++ src/main/java/com/uis/exception/InValidInputException.java
@@ -0,0 +1,31 @@
+package com.uis.exception;
+
+public class InValidInputException extends RuntimeException {
+
+	public InValidInputException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InValidInputException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InValidInputException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InValidInputException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InValidInputException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+  
+}
--- src/main/java/com/uis/mvn/String1.java
+++ src/main/java/com/uis/mvn/String1.java
@@ -1,31 +1,37 @@
 package com.uis.mvn;
+
 import java.util.Scanner;
 import org.apache.commons.lang3.StringUtils;
+import com.uis.exception.InValidInputException;
 public class String1 {
 
 	public static void main(String[] args) {
-          Scanner kb=new Scanner(System.in);
-          System.out.println("enter the name");
-          String name=kb.nextLine();
- 	is20Character(name);
- 	kb.close();
+		Scanner kb = new Scanner(System.in);
+		System.out.println("enter the name");
+		String name = kb.nextLine();
+		is20Character(name);
+		kb.close();
 	}
-     public static void is20Character(String name) {
-    	 if (name.length()==20) {
-    			System.out.println(name);
-    		} else if(name.length()>20) {
-    			String dupName=name;
-    	      for (int i = 1; i <=(name.length()-20); i++) {
-    			dupName=StringUtils.chop(dupName);
-    		}
-    	      System.out.println(dupName);
-    		}
-    		else if(name.length()<20) {
-    			for (int i = 1; i <=(20-(name.length())); i++) {
-    				System.out.print("*");
-    			}
-    			System.out.println(name);
-    		}
-      }
-     
+
+	public static void is20Character(String name) {
+		if (name.trim().isEmpty() ) {
+			throw new InValidInputException("please enter proper input");
+		}
+		
+		if (name.length() == 20) {
+			System.out.println(name);
+		} else if (name.length() > 20) {
+			String dupName = name;
+			for (int i = 1; i <= (name.length() - 20); i++) {
+				dupName = StringUtils.chop(dupName);
+			}
+			System.out.println(dupName);
+		} else if (name.length() < 20) {
+			for (int i = 1; i <= (20 - (name.length())); i++) {
+				System.out.print("*");
+			}
+			System.out.println(name);
+		}
+	}
+
 }
