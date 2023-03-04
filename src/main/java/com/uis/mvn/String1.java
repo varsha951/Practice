2721adb4b8eefc00c4090850ab446d58134c7030
varsha <varsha95155@gmail.com>
2023-03-04 12:39:53 +0530

    profanity check

--- src/main/java/com/uis/mvn/String1.java
+++ src/main/java/com/uis/mvn/String1.java
@@ -3,35 +3,63 @@ package com.uis.mvn;
 import java.util.Scanner;
 import org.apache.commons.lang3.StringUtils;
 import com.uis.exception.InValidInputException;
+
 public class String1 {
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		System.out.println("enter the name");
 		String name = kb.nextLine();
-		is20Character(name);
+		System.out.println(is20Character(name));
+		System.out.println(profanityCheck(name));
 		kb.close();
 	}
 
-	public static void is20Character(String name) {
-		if (name.trim().isEmpty() ) {
+	public static String is20Character(String name) {
+		if (name.trim().isEmpty()) {
 			throw new InValidInputException("please enter proper input");
 		}
-		
+		StringBuffer sb = new StringBuffer();
 		if (name.length() == 20) {
-			System.out.println(name);
+			sb = sb.append(name);
 		} else if (name.length() > 20) {
 			String dupName = name;
 			for (int i = 1; i <= (name.length() - 20); i++) {
 				dupName = StringUtils.chop(dupName);
 			}
-			System.out.println(dupName);
+			sb = sb.append(dupName);
 		} else if (name.length() < 20) {
 			for (int i = 1; i <= (20 - (name.length())); i++) {
-				System.out.print("*");
+				sb = sb.append("*");
 			}
-			System.out.println(name);
+			sb = sb.append(name);
 		}
+
+		String s = sb.toString();
+		return s;
 	}
 
+	public static String profanityCheck(String name) {
+		if (name.trim().isEmpty()) {
+			throw new InValidInputException("please enter proper input");
+		}
+		String[] profanity = { "idiot", "mad", "mental" };
+		StringBuffer sb = new StringBuffer();
+		String[] ss = new String[profanity.length];
+
+		for (int i = 0; i < profanity.length; i++) {
+			char[] c1 = profanity[i].toCharArray();
+			for (int j = 0; j < c1.length; j++) {
+				sb = sb.append("*");
+			}
+			ss[i] = sb.toString();
+			sb = new StringBuffer();
+		}
+
+		for (int i = 0; i < profanity.length; i++) {
+			name = StringUtils.replaceIgnoreCase(name, profanity[i], ss[i]);
+		}
+
+		return name;
+	}
 }
